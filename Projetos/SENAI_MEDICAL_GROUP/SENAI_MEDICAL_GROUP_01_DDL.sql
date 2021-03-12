f25fc880026cdaf7fd2ff923f4279d65c2693740
MatheusStrilicherkDEV <matheuscherk@hotmail.com>
2021-03-12 10:50:10 -0300

    Modelos Adicionados, alteração pendente no físico

---
+++ Projetos/SENAI_MEDICAL_GROUP/SENAI_MEDICAL_GROUP_01_DDL.sql
@@ -0,0 +1,12 @@
+--DDL
+
+-- Criando o banco de dados
+CREATE DATABASE MEDICAL_GROUP_TARDE
+GO
+
+-- Selecionando o banco de dados que será utilizado
+USE MEDICAL_GROUP_TARDE
+GO
+
+-- Criando as tabelas que farão parte do nosso banco de dados
+CREATE TABLE 
